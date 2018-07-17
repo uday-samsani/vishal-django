a9ed14024106d0963f640dedff135e7ebc0cce80
uday-samsani <satyasaisamsani@gmail.com>
2018-07-17 18:06:06 +0530

    'migrations'

--- portal/urls.py
+++ portal/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views as portalView
 
 urlpatterns=[
-    path('',portalView.index,name='index')
+    path('',portalView.index,name='index'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import AlumniInfo
 
 def index(request):
-    return render(request, template_name=index.html,context={'AlumniInfo':AlumniInfo.objects.all()})
+    count=AlumniInfo.objects.all().count()
+    return render(request, template_name='index.html',context={'AlumniInfo':count})
     # using template index.html in portal/templates and context i.e. model info is passed out to views render
