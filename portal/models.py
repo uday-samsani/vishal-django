60b9c8049bd43b284eadf6bc86f74653156cb338
murthychavva <murthychavva1997@gmail.com>
2018-07-17 17:39:01 +0530

    model

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-class
+class AlumniInfo(models.Model):
+    name = models.CharField(max_length = 32)
+    reg_no = models.CharField(max_length = 8)
+    mail_id = models.CharField(max_length = 16)
+    degree = models.CharField(max_length = 16)
+    department = models.CharField(max_length=32)
+    company= models.CharField(max_length=64)
+    experience = models.IntegerField(max_length=2)
+    location = models.CharField(max_length=16)
+    linkedin = models.CharField(max_length=16)
+    github = models.CharField(max_length=16)
+    working_as = models.CharField(max_length=16)
+    description = models.TextField(null=True, blank=True)
