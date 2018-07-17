8df7e37f12bc3b2e42f29600f45f8dea09ef1d94
uday-samsani <satyasaisamsani@gmail.com>
2018-07-18 04:53:24 +0530

    'add:profile'

--- portal/urls.py
+++ portal/urls.py
@@ -3,5 +3,9 @@ from . import views as portalView
 
 urlpatterns=[
     path('',portalView.index,name='index'),
-    path('submit/',portalView.submit_info,name='submit_info')
+    path('submit/',portalView.submit_info,name='submit_info'),
+    path('thanks/',portalView.thanks,name='thanks'),
+    path('ListFeed/',portalView.ListFeed.as_view(template_name='ListFeed.html'),name='list-feed'),
+    path(r'profile/(?P<name>\d+).$',portalView.profile,name='profile')
+    # path('ListDetailed',portalView.ListDetailed.as_view(template_name=''))
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,14 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import AlumniInfo
 from .forms import AlumniInfoForm
+from django.views.generic import ListView,DetailView
+from django.db.models import Subquery
 
 def index(request):
     count=AlumniInfo.objects.all().count()
     return render(request, template_name='index.html',context={'AlumniInfo':count})
     # using template index.html in portal/templates and context i.e. model info is passed out to views render
 
+def thanks(request):
+    return HttpResponse("Thanks")
 
 def submit_info(request):
-    form=AlumniInfoForm()
-    return render(request,"submit.html",{'form':form})
+    if request.method=='POST':
+        form = AlumniInfoForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.save()
+            return redirect('thanks')
+    else:
+        form = AlumniInfoForm()
+    return render(request,template_name= 'submit.html',context={'form':form})
+
+class ListFeed(ListView):
+
+    model = AlumniInfo
+    paginate_by = 15  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+'''
+class ListDetailed(DetailView):
+
+    model = AlumniInfo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+'''
+def profile(request,name1):
+
+   record=AlumniInfo.objects.filter(name=name1)
+   return render(request,template_name='profile.html',context={'record':record})
+   # return HttpResponse('Hi this is a profile of {}'.format(name))
+
+
+
